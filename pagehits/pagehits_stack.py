95502191fd11803a45e298f423e689c2016dc52a
Matthew Mueller <mmueller@viedu.org>
2022-06-24 14:37:02 -0700

    added lambda rest API

--- pagehits/pagehits_stack.py
+++ pagehits/pagehits_stack.py
@@ -1,7 +1,7 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_apigateway as apigateway,
+    aws_lambda as lambda_,
 )
 from constructs import Construct
 
@@ -10,10 +10,29 @@ class PagehitsStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        backend = lambda_.Function(
+            self, "pagehitsLambda",
+            runtime=lambda_.Runtime.PYTHON_3_9,
+            handler="index.handler",
+            code=lambda_.Code.from_inline('print("test")'))
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "PagehitsQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        backend.apply_removal_policy(RemovalPolicy.DESTROY)
+
+        api = apigateway.LambdaRestApi(
+            self, "pagehitsRestApi",
+            handler=backend,
+            proxy=False
+        )
+
+        api.apply_removal_policy(RemovalPolicy.DESTROY)
+
+        items = api.root.add_resource("items")
+        items.add_method("GET") # GET /items
+        items.add_method("POST") # POST /items
+
+        item = items.add_resource("{item}")
+        item.add_method("GET") # GET /items/{item}
+
+        # the default integration for methods is "handler", but one can
+        # customize this behavior per method or even a sub path.
+        item.add_method("DELETE", apigateway.HttpIntegration("http://amazon.com"))
